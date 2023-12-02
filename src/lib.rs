1b677bfdabf501f6f850263538b8fb6bec3ca785
Panik <ikermumolero@gmail.com>
2023-12-02 11:34:42 +0100

    Empty project.
    Includes:
    - Default "Cargo.toml".
    - Empty library with a description of the project.

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+//! A Rust package that provides utility functions inspired by or ported from other programming languages.
+//! 
+//! rusty_utils aims to simplify intricate and hard-to-read instructions in Rust, allowing you to express complex operations with clarity and brevity.
+//! Inspired by the conciseness of other programming languages, rusty_utils provides a set of functions that compact multiple steps into a single, readable call.
